751942a2b803987099675d23048c5bf639864687
Paola Taboada <taboadapaola3@gmail.com>
2025-02-14 12:40:25 -0500

    feat: add pagination for pets

--- src/presentation/pages/PetsPage.tsx
+++ src/presentation/pages/PetsPage.tsx
@@ -1,17 +1,29 @@
+import ReactPaginate from "react-paginate";
 import { usePets } from "../hooks/usePets";
+import { useState } from "react";
 
 const PetsPage = () => {
     const { pets, loading } = usePets();
 
-    if (loading) return <div className="text-center p-4">Loading...</div>;
+    const [itemOffset, setItemOffset] = useState(0);
+    const itemsPerPage = 8;
+    const endOffset = itemOffset + itemsPerPage;
+    const currentItems = pets.slice(itemOffset, endOffset);
+    const pageCount = Math.ceil(pets.length / itemsPerPage);
 
+    const handlePageClick = (event: { selected: number }) => {
+        const newOffset = (event.selected * itemsPerPage) % pets.length;
+        setItemOffset(newOffset);
+    };
+
+    if (loading) return <div className="text-center p-4">Loading...</div>;
 
     return (
         <div className="p-4">
             <h1 className="text-xl font-bold mb-4">Pets</h1>
             <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                {pets.map((pet) => (
-                    <div key={pet.id} className="group relative border border-gray-200 rounded-md">
+                {currentItems.map((pet, index) => (
+                    <div key={index} className="group relative border border-gray-200 rounded-md">
                         <img
                             alt={pet.name}
                             src={pet.photoUrls[0]}
@@ -19,10 +31,7 @@ const PetsPage = () => {
                         />
                         <div className="py-3 px-5 flex justify-between items-center">
                             <h3 className="font-medium text-sm text-gray-400 capitalize truncate">
-                                <a href={pet.name}>
-                                    <span aria-hidden="true" className="absolute inset-0" />
-                                    {pet.name}
-                                </a>
+                                {pet.name}
                             </h3>
                             <p className="font-medium text-xs text-gray-500 bg-gray-900 rounded-4xl py-1 px-2">
                                 {pet.status}
@@ -40,6 +49,29 @@ const PetsPage = () => {
                         </div>
                     </div>
                 ))}
+            </div>
+            <div className="my-6">
+                <ReactPaginate
+                    nextLabel="Next"
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={3}
+                    marginPagesDisplayed={2}
+                    pageCount={pageCount}
+                    previousLabel="Previous"
+                    pageClassName="page-item"
+                    pageLinkClassName="block border border-white w-10 text-center py-2 cursor-pointer select-none"
+                    previousClassName="page-item"
+                    previousLinkClassName="block border border-white rounded-l-md w-22 text-center py-2 cursor-pointer select-none"
+                    nextClassName="page-item"
+                    nextLinkClassName="block border border-white rounded-r-md w-22 text-center py-2 cursor-pointer select-none"
+                    breakLabel="..."
+                    breakClassName="page-item"
+                    breakLinkClassName="block border border-white w-10 text-center py-2"
+                    containerClassName="pagination"
+                    activeClassName="bg-white text-gray-900 font-medium underline underline-offset-3"
+                    className="flex justify-center"
+                    renderOnZeroPageCount={null}
+                />
             </div>
         </div>
     )
